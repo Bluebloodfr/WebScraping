4bb107d84b213ce84737915573cab3b0e6ccfd56
Bluebloodfr <louismelchiorgiraud@gmail.com>
2024-12-11 09:41:28 +0100

    Begun adding google maps API

--- back/gmaps/main.py
+++ back/gmaps/main.py
@@ -7,9 +7,11 @@ import geopandas as gpd
 import plotly.express as px
 import requests
 from bs4 import BeautifulSoup
+import googlemaps
 
 load_dotenv()
 ROOT_DIR = os.getenv('ROOT_DIR')
+API_KEY = os.getenv('GOOGLE_API_KEY')
 
 ##########################
 ########## POI ###########
@@ -134,21 +136,23 @@ weather_dict = {
 ##########################
 
 def get_gmaps_reviews(place_id):
-    reviews = []
-    url = f"https://www.google.com/maps/place/?q=place_id:{place_id}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    gmaps = googlemaps.Client(key=API_KEY)
+    place_details = gmaps.place(place_id=place_id, fields=['reviews'])
+    reviews = place_details.get('result', {}).get('reviews', [])
     
-    for review in soup.find_all('div', class_='review'):
-        reviewer = review.find('div', class_='reviewer').text
-        rating = review.find('span', class_='rating').text
-        text = review.find('span', class_='review-text').text
-        reviews.append({'reviewer': reviewer, 'rating': rating, 'text': text})
+    reviews_data = []
+    for review in reviews:
+        reviews_data.append({
+            'author_name': review.get('author_name'),
+            'rating': review.get('rating'),
+            'text': review.get('text'),
+            'time': review.get('relative_time_description')
+        })
     
-    df_reviews = pd.DataFrame(reviews)
+    df_reviews = pd.DataFrame(reviews_data)
     return df_reviews
 
 # Example usage
-# place_id = 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-# reviews_df = get_gmaps_reviews(place_id)
-# print(reviews_df)
+place_id = 'ChIJdUyx15R95kcRj85ZX8H8OAU' # Chateau de Versailles
+reviews_df = get_gmaps_reviews(place_id)
+print(reviews_df)
