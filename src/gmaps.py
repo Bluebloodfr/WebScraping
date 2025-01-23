e349eeb6c92403d5ee31d5e04be6ee615a06b71b
Bluebloodfr <louismelchiorgiraud@gmail.com>
2025-01-23 15:16:27 +0100

    Cleaned the file a bit

--- src/gmaps.py
+++ src/gmaps.py
@@ -74,14 +74,6 @@ def get_gmaps_reviews(place_id):
     return df_reviews
 
 def get_gmaps_reviews_by_name(name, latitude, longitude, debug=False):
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.firefox.options import Options
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    import time
-    import re
 
     options = Options()
     options.headless = True
